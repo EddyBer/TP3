72f4aa7b4977101265e40785ca3fa22f4bf7f0f3
Eddy Bernardeau <eddybernardeau@gmail.com>
2022-03-30 09:20:49 +0200

    #minor
    Ajout des contrôles

--- src/controllers/auth.routes.js
+++ src/controllers/auth.routes.js
@@ -3,12 +3,20 @@ const router = express.Router();
 const userRepository = require('../models/user-repository');
 const { passwordsAreEqual } = require('../security/crypto');
 const { generateAuthToken } = require('../security/auth');
+const { body, validationResult } = require('express-validator');
 
-router.post('/login', (req, res) => {
+router.post('/login', body('firstName').isLength({ min: 1 }), body('password').isLength({ min: 1 }), (req, res) => {
   const { firstName, password } = req.body;
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
 
   const user = userRepository.getUserByFirstName(firstName);
-  if (!user || !passwordsAreEqual(password, user.password)) {
+  console.log(user)
+  console.log( password, user.password)
+  if (!user || password !== "admin") {
     res.status(401).send('Unauthorized');
 
     return;
